8e43153e347655b22232401d67a1b8308d54f0d1
joseph <langj6757@gmail.com>
2024-08-11 11:49:38 -0700

    Remove extra imports in navbar component

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,11 +1,5 @@
 "use client";
-import React from "react";
-import Logo from "../../assets/logo.svg";
-import CaretDown from "../../assets/caret-down.svg";
-import Hamburger from "hamburger-react";
 import { useState } from "react";
-import { navItems } from "../../constants/index";
-import Link from "next/link";
 import DesktopNav from "./components/desktop-nav";
 import MobileDropdown from "./components/mobile-dropdown";
 const Navbar = () => {
