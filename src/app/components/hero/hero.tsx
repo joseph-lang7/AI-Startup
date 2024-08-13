6bf720a887354a10e182cbea8988ff8703cba6a3
joseph <langj6757@gmail.com>
2024-08-12 21:27:01 -0700

    Implement features section & adjust hero background design

--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -4,10 +4,10 @@ const Hero = () => {
   return (
     <section className="pt-[90px]">
       <div className="flex flex-col gap-5 relative">
-        <div className="hidden md:block absolute inset-0 bg-gradient-to-t from-purple-500/50 to-purple-700/50 rounded-lg -z-10"></div>
-        <div className="hidden md:block absolute inset-0 bg-gradient-to-r from-black via-purple-700/50 to-black rounded-lg -z-10"></div>
-        <div className="hidden md:block absolute inset-0 bg-gradient-to-l from-black via-purple-700/50 to-black rounded-lg -z-10"></div>
-        <div className="hidden md:block absolute inset-0 bg-gradient-to-b from-black via-purple-700/50 to-black rounded-lg -z-10"></div>
+        <div className="hidden sm:block absolute inset-0 bg-gradient-to-t from-purple-500/50 to-purple-700/50 rounded-lg -z-10"></div>
+        <div className="hidden sm:block absolute inset-0 bg-gradient-to-r from-black via-purple-700/50 to-black rounded-lg -z-10"></div>
+        <div className="hidden sm:block absolute inset-0 bg-gradient-to-l from-black via-purple-700/50 to-black rounded-lg -z-10"></div>
+        <div className="hidden sm:block absolute inset-0 bg-gradient-to-b from-black via-purple-700/50 to-black rounded-lg -z-10"></div>
         <div className="flex justify-center">
           <div className="text-[#9855FF] outline outline-white/15 outline-offset-4 w-[291px] h-[42px] rounded-2xl text-[16px] flex items-center justify-center">
             <span className="bg-[#9855FF] text-black font-bold rounded-xl px-1 mr-2">
@@ -16,13 +16,31 @@ const Hero = () => {
             Latest integration just arrived
           </div>
         </div>
-        <div className="text-white text-center px-[16px] flex flex-col gap-5 items-center">
-          <h1 className="font-medium text-[48px] md:text-[82px]">
-            Boost your <br />{" "}
-            <span className="bg-gradient-to-b from-white to-[#B372CF] bg-clip-text text-transparent">
-              rankings with AI.
-            </span>
-          </h1>
+        <div className="text-white text-center px-[16px] flex flex-col gap-5 items-center sm:overflow-hidden">
+          <div className="relative">
+            <div className="translate-y-[750px] md:translate-y-[880px] ">
+              <div className="hidden md:inline-block h-[500px] w-[500px] rounded-full border absolute bottom-0 left-1/2 -translate-x-1/2 opacity-10 -z-10"></div>
+              <div className="hidden md:inline-block h-[600px] w-[600px] rounded-full border border-dashed absolute bottom-0 left-1/2 -translate-x-1/2 opacity-10 -z-10"></div>
+              <div className="hidden md:inline-block h-[750px] w-[750px] rounded-full border absolute bottom-0 left-1/2 -translate-x-1/2 opacity-10 -z-10">
+                <div className="h-[20px] w-[20px] rounded-full bg-transparent border fixed top-0 left-0 -translate-x-[-64px] -translate-y-[-140px]">
+                  <div className="h-[10px] w-[10px] rounded-full bg-transparent border fixed top-0 left-0 -translate-x-[-4px] -translate-y-[-4px]"></div>
+                </div>
+              </div>
+              <div className="hidden md:hidden sm:inline-block h-[500px] w-[500px] rounded-full border absolute bottom-0 left-1/2 -translate-x-1/2 opacity-10 -z-10"></div>
+              <div className="hidden md:hidden sm:inline-block h-[600px] w-[600px] rounded-full border border-dashed absolute bottom-0 left-1/2 -translate-x-1/2 opacity-10 -z-10"></div>
+              <div className="hidden md:hidden sm:inline-block h-[700px] w-[700px] rounded-full border absolute bottom-0 left-1/2 -translate-x-1/2 opacity-10 -z-10">
+                <div className="h-[20px] w-[20px] rounded-full bg-transparent border fixed top-0 left-0 -translate-x-[-120px] -translate-y-[-67px]">
+                  <div className="h-[10px] w-[10px] rounded-full bg-transparent border fixed top-0 left-0 -translate-x-[-4px] -translate-y-[-4px]"></div>
+                </div>
+              </div>
+            </div>
+            <h1 className="font-medium text-[48px] md:text-[82px]">
+              Boost your <br />{" "}
+              <span className="bg-gradient-to-b from-white to-[#B372CF] bg-clip-text text-transparent">
+                rankings with AI.
+              </span>
+            </h1>
+          </div>
           <p className="max-w-[540px]">
             Elevate your site’s visibility effortlessly with AI, where smart
             technology meets user-friendly SEO tools.
@@ -32,19 +50,12 @@ const Hero = () => {
           </button>
           <div className="w-full h-full flex justify-center">
             <div className="relative">
-              <div className="hidden md:inline-block h-[500px] w-[500px] rounded-full border absolute bottom-0 left-1/2 -translate-x-1/2 opacity-10 -z-10"></div>
-              <div className="hidden md:inline-block h-[600px] w-[600px] rounded-full border border-dashed absolute bottom-0 left-1/2 -translate-x-1/2 opacity-10 -z-10"></div>
-              <div className="hidden md:inline-block h-[700px] w-[700px] rounded-full border absolute bottom-0 left-1/2 -translate-x-1/2 opacity-10 -z-10">
-                <div className="h-[20px] w-[20px] rounded-full bg-transparent border absolute top-0 left-0 -translate-x-[-52px] -translate-y-[-140px]">
-                  <div className="h-[10px] w-[10px] rounded-full bg-transparent border absolute top-0 left-0 -translate-x-[-2.8px] -translate-y-[-3.5px]"></div>
-                </div>
-              </div>
               <div className="md:hidden absolute inset-0 bg-gradient-to-b from-purple-500 to-purple-700/80 rounded-lg blur-2xl -z-10"></div>
               <div className="md:block hidden absolute inset-0 bg-gradient-to-b from-purple-500 to-black rounded-lg blur-2xl -z-10"></div>
               <Image
-                src="/app.png"
-                height="1120"
-                width="815"
+                src="/images/app.png"
+                height="815"
+                width="1120"
                 alt="app mockup"
                 className="bg-black/30"
               />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./components/hero/hero";
 import Companies from "./components/companies/companies";
+import Features from "./components/features/features";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Companies />
+      <Features />
     </>
   );
 }
