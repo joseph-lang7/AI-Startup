cf3f62ece4cf62d32811a6d1a3ad6de33da40195
Joseph <langj6757@gmail.com>
2024-08-21 18:12:35 -0700

    Fix layout legacy prop warning

--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 const Hero = () => {
   return (
     <section className="pt-[90px]">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Hero from "./components/hero/hero";
 import Companies from "./components/companies/companies";
 import Features from "./components/features/features";
